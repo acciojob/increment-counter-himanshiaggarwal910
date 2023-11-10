abe490a1890e75c1ab70cdc81f40cfd4b24243c6
himanshiaggarwal910 <141843813+himanshiaggarwal910@users.noreply.github.com>
2023-11-11 01:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let p=document.querySelector("#counter");
+let btn=document.querySelector("#incrementBtn");
+btn.addEventListener("click",onclick);
+ let i=0;
+
+function onclick(){
+	alert(p.innerText);
+	i++;
+	p.innerText=i;	
+}
